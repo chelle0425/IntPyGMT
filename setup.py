f64384cded3ea852a6d42b2e779e43ab908bf0e5
Rochelle Pun <128195508+chelle0425@users.noreply.github.com>
2024-07-14 04:32:52 +0100

    initial commit
    untested

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='IntPyGMT',
+    version='0.1.0',
+    description='An interactive map tool for pyGMT',
+    url='https://github.com/chelle0425/interactive_pygmt',
+    author='Rochelle Pun',
+    author_email='rochelle.pun@gmail.com',
+    packages=['IntPyGMT'],
+    package_dir = {'IntPyGMT':'python'},
+    install_requires = [
+        'numpy',
+        'matplotlib',
+        'ipympl',
+        'basemap',
+        'pillow']
+)
