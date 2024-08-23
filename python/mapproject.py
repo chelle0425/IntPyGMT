d6709b9847cea238ee9d0e3bd9a08f66126657bf
Rochelle Pun <128195508+chelle0425@users.noreply.github.com>
2024-08-23 12:24:39 +0100

    mapproject test
    I hope nothing breaks...

--- python/mapproject.py
+++ python/mapproject.py
@@ -13,12 +13,22 @@ from pygmt.helpers import build_arg_list, fmt_docstring, kwargs_to_strings, use_
     R="region",
     J="projection",
 )
-@kwargs_to_strings(R="sequence", c="sequence_comma", p="sequence")
-def mapproject(self, **kwargs):
+@kwargs_to_strings(R="sequence")
+def mapproject(
+    self, x=None, y=None, **kwargs):
     r"""
     Reads (lon, lat) positions from tables [or standard input] and computes (x, y) coordinates using the specified map projection and scales.
     Optionally, it can read (x, y) positions and compute (lon, lat) values doing the inverse transformation. 
+
+    Must provide ``x``/``y``.
+
+    THIS IS MODIFIED TO MAKE IT WORK WITH MY CODE SOMEONE PLEASE MAKE THIS WORK PROPERLY
     """
     kwargs = self._preprocess(**kwargs)
+
+
     with Session() as lib:
-        lib.call_module(module="mapproject", args=build_arg_list(kwargs))
+        with lib.virtualfile_in(
+            x=x, y=y
+        ) as vintbl:
+            lib.call_module(module="mapproject", args=build_arg_list(kwargs, infile=vintbl))
